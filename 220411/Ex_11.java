26836cf189379c33f35fe8965a8c87faff83b633
DEUKHEE <101791490+santoNNrini0078@users.noreply.github.com>
2022-04-12 14:01:06 +0900

    Update Ex_11.java

--- 220411/Ex_11.java
+++ 220411/Ex_11.java
@@ -4,38 +4,38 @@ public class Ex_11 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		int[] a = new int[] {10, 40, 20, 33, 8};
+		MyData ret = minMax(a);
+		System.out.println(ret.max);
+		System.out.println(ret.min);
 		
 	}
 
-}
-class Mydata
-{
-	int[] a;
-	
-	int min()
+	static MyData minMax(int[] array)
 	{
-		int min = a[0];	
-		for(int i=0; i<a.length; i++)
-		{
-			if (min>a[i])
-			{
-				min=a[i];
-			}			
-		}
-		return min;
-	}
-		
-	int max()
+		//최대값과 최소값을 구해서
+		MyData data = new MyData();
+		data.min = array[0];
+		data.max = array[0];
+		for(int i=0; i<array.length; i++)
 		{			
-			int max = a[0];
-			for(int i=0; i<a.length; i++)
-			{				
-				if (max <a[i])
-				{
-					max=a[i];
-				}
-			}		
-			return max;
+			if (data.min>array[i])
+			{
+				data.min=array[i];
+			}
+			
+			if (data.max <array[i])
+			{
+				data.max=array[i];
+			}
+		}
+		return data;
+		
 	}
+}
+class MyData
+{
+	int min;
+	int max;
 	
 }
